1af1381c16d9c222b428bac8af27c0a36c89aa52
MD Shimul <mdshemul480@gmail.com>
2021-10-18 11:29:03 +0600

    adding links to the navbar

--- src/components/Shared/NavBar/NavBar.js
+++ src/components/Shared/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink, Link } from 'react-router-dom';
 
 import logo from '../../../assets/logo.png';
 
@@ -8,16 +9,24 @@ const NavBar = () => {
   return (
     <Navbar expand='lg' variant='dark' className='navbar'>
       <Container>
-        <Navbar.Brand href='/'>
+        <Navbar.Brand as={Link} to='/'>
           <img src={logo} alt='website logo' />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='navbarScroll' />
         <Navbar.Collapse id='navbarScroll'>
           <Nav className='ms-auto'>
-            <Nav.Link href='#'>Home</Nav.Link>
-            <Nav.Link href='#'>Services</Nav.Link>
-            <Nav.Link href='#'>Contact us</Nav.Link>
-            <Nav.Link href='#'>About Us</Nav.Link>
+            <Nav.Link as={NavLink} to='/'>
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to='/services'>
+              Services
+            </Nav.Link>
+            <Nav.Link as={NavLink} to='/contact-us'>
+              Contact us
+            </Nav.Link>
+            <Nav.Link as={NavLink} to='/about'>
+              About Us
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
