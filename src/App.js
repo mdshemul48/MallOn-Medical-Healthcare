510de5b83eb1b88d24cb628acfa071668c667611
MD Shimul <mdshemul480@gmail.com>
2021-10-18 11:00:36 +0600

    adding all the pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+// all the main components
 import Home from './components/Home/Home';
-
+import Services from './components/Services/Services';
+import SingleService from './components/Services/SingleService';
+import About from './components/About/About';
+import ContactUs from './components/ContactUs/ContactUs';
 import './App.css';
-
 function App() {
   return (
     <Router>
       <Switch>
         <Route path='/' component={Home} exact />
+        <Route path='/services/:serviceId' component={SingleService} />
+        <Route path='/services' component={Services} />
+        <Route path='/contact-us' component={ContactUs} />
+        <Route path='/about' component={About} />
       </Switch>
     </Router>
   );
---
+++ src/components/ContactUs/ContactUs.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ContactUs = () => {
+  return <div>this is contact us</div>;
+};
+
+export default ContactUs;
