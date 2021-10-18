d705d08eac253aa7ced52bbc20e4ea9aae22ec07
MD Shimul <mdshemul480@gmail.com>
2021-10-18 11:11:44 +0600

    nav bar content

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import SingleService from './components/Services/SingleService';
 import About from './components/About/About';
 import ContactUs from './components/ContactUs/ContactUs';
 
+import NavBar from './components/Shared/NavBar/NavBar';
 // styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 function App() {
   return (
     <Router>
+      <NavBar />
       <Switch>
         <Route path='/' component={Home} exact />
         <Route path='/services/:serviceId' component={SingleService} />
--- src/components/Shared/NavBar/NavBar.js
+++ src/components/Shared/NavBar/NavBar.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Navbar bg='light' expand='lg'>
+        <Navbar.Brand href='#'>Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbarScroll' />
+        <Navbar.Collapse id='navbarScroll'>
+          <Nav>
+            <Nav.Link href='#action1'>Home</Nav.Link>
+            <Nav.Link href='#action2'>Services</Nav.Link>
+            <Nav.Link href='#action2'>Contact us</Nav.Link>
+            <Nav.Link href='#action2'>About Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Container>
+  );
 };
 
 export default NavBar;
