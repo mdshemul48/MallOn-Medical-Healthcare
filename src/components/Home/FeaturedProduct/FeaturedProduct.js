8e4d3dc5e23af03a4f31ffa44471e9e04dda8c0d
MD Shimul <mdshemul480@gmail.com>
2021-10-18 17:44:18 +0600

    ading product to the home page

--- src/components/Home/FeaturedProduct/FeaturedProduct.js
+++ src/components/Home/FeaturedProduct/FeaturedProduct.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 
+import { AiOutlineArrowRight } from 'react-icons/ai';
+
 import useProduct from '../../../Hooks/useProducts';
 
 import Product from './Product';
 
 import './FeaturedProduct.css';
+import { Link } from 'react-router-dom';
 const FeaturedProduct = () => {
   const { products } = useProduct();
   console.log(products);
   return (
-    <Container>
+    <Container className='featured-products'>
       <p>FEATURED PRODUCTS</p>
-      <div>
-        <h1>A Few Of Our Favorites.</h1>
+      <div className='d-flex justify-content-between align-items-center'>
+        <h1 className='my-4'>A Few Of Our Favorites.</h1>
+        <Link
+          to='/services'
+          className='btn text-uppercase text-bold all-services-button'
+        >
+          All Products <AiOutlineArrowRight />
+        </Link>
       </div>
       <div>
         <Row className='g-3'>
-          {products.map((product) => (
+          {products.slice(0, 6).map((product) => (
             <Product key={product.id} product={product} />
           ))}
         </Row>
--- src/components/Home/FeaturedProduct/Product.js
+++ src/components/Home/FeaturedProduct/Product.js
@@ -11,13 +11,13 @@ const Product = ({ product: { name, description, img, id } }) => {
           <img src={img} alt={name} width='200px' />
         </div>
         <div className='product-info'>
-          <h5>{name.substring(0, 40)}..</h5>
+          <h5>{name}</h5>
           <p>{description.substring(0, 100)}...</p>
           <Link
             to={`/services/${id}`}
             className='btn product-button text-light'
           >
-            View Product
+            View Details
           </Link>
         </div>
       </div>
