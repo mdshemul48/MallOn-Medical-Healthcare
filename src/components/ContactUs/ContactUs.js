b01cf13412f5e228b3a34d760da29da33fa23869
MD Shimul <mdshemul480@gmail.com>
2021-10-19 11:08:15 +0600

    adding contact page

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import {
+  Button,
+  Col,
+  Container,
+  FloatingLabel,
+  Form,
+  Row,
+} from 'react-bootstrap';
 import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 
 const ContactUs = () => {
@@ -25,7 +32,40 @@ const ContactUs = () => {
           </div>
         </Col>
         <Col lg={6}>
-          <h2>Contact Us</h2>
+          <h2>Contact us</h2>
+          <Form>
+            <Row className='mb-3'>
+              <Form.Group as={Col} controlId='formGridEmail'>
+                <Form.Label>First Name</Form.Label>
+                <Form.Control type='text' placeholder='Enter Your First Name' />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId='formGridPassword'>
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control type='text' placeholder='Enter Your Last name' />
+              </Form.Group>
+            </Row>
+
+            <Form.Group className='mb-3' controlId='formGridAddress1'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control type='email' placeholder='someone@mail.com' />
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formGridAddress2'>
+              <Form.Label>Your Massage</Form.Label>
+              <FloatingLabel controlId='floatingTextarea2' label='Your Massage'>
+                <Form.Control
+                  as='textarea'
+                  placeholder='Leave a message here'
+                  style={{ height: '100px' }}
+                />
+              </FloatingLabel>
+            </Form.Group>
+
+            <Button variant='primary' type='submit'>
+              Send
+            </Button>
+          </Form>
         </Col>
       </Row>
     </Container>
