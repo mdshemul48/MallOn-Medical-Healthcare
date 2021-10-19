3bb9790d377482c3c6bcf5129181704b95497096
MD Shimul <mdshemul480@gmail.com>
2021-10-19 11:41:06 +0600

    adding not found page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import NotFoundImage from '../../assets/img-404.jpg';
 
+import './NotFound.css';
 const NotFound = () => {
-  return <div>not found 404</div>;
+  return (
+    <Container className='not-found'>
+      <img className='img-fluid' src={NotFoundImage} alt='not found page' />
+      <div className='text-center my-5'>
+        <h1>The page you requested cannot be found!</h1>
+        <p className='mt-3'>The page you requested cannot be found.</p>
+      </div>
+    </Container>
+  );
 };
 
 export default NotFound;
